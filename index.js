c2564ebac318e1a47db3ae1b3e43873497ad3fe2
tlamont06 La <tlamont06@gmail.com>
2019-02-18 02:26:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function incrementVariable() {
   return i;
 }
   let array = [];
-  i = 15 - num;
+  i = i + 1;
   do{array.push('I love the Beatles!')
 } while(incrementVariable() < num);
 return array;
