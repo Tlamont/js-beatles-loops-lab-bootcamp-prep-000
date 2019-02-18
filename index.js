9ba5ead9a2e86a658172b7907a3ff48058a57cb4
tlamont06 La <tlamont06@gmail.com>
2019-02-18 01:55:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  var i = 0;
+  for(i = 0; i < musicians.length; i++){
+  array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+return array;
+}
+
+function johnLennonFacts(jLFacts){
+  let array = [];
+  let i = 0;
+  while(i < jLFacts.length){
+    array.push(`${jLFacts}!!!`)
+  }
+  return array;
+}
